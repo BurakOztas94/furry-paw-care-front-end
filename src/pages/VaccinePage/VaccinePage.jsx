909a71b8346ecb5a0c6a07465811af887c04b54d
Burak Öztaş <143541613+BurakOztas94@users.noreply.github.com>
2024-03-10 16:17:32 +0300

    Create VaccinePage.jsx

---
+++ src/pages/VaccinePage/VaccinePage.jsx
@@ -0,0 +1,11 @@
+import "./VaccinePage.style.css"
+
+const VaccinePage = () => {
+    return (
+        <div className="vaccine-page">
+
+        </div>
+    )
+                
+}
+export default VaccinePage
