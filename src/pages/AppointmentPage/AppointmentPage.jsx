494c3f62469634ae94ba049adc1f5c4e279a670a
Burak Öztaş <143541613+BurakOztas94@users.noreply.github.com>
2024-03-10 16:17:11 +0300

    Create AppointmentPage.jsx

---
+++ src/pages/AppointmentPage/AppointmentPage.jsx
@@ -0,0 +1,11 @@
+import "./AppointmentPage.style.css"
+
+const AppointmentPage = () => {
+    return (
+        <div className="appoointment-page">
+
+        </div>
+    )
+                
+}
+export default AppointmentPage
