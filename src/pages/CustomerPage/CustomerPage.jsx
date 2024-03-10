baee032f43274540a1a8a6c5e2136ea786bc3077
Burak Öztaş <143541613+BurakOztas94@users.noreply.github.com>
2024-03-10 17:22:34 +0300

    Update CustomerPage.jsx

--- src/pages/CustomerPage/CustomerPage.jsx
+++ src/pages/CustomerPage/CustomerPage.jsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from "react"
 import "./CustomerPage.style.css"
+import { getCustomers } from "../../api/CustomerService"
+import CustomerCard from "../../components/Customer/CustomerCard/CustomerCard"
 
 const CustomerPage = () => {
+    const [customers, setCustomers] = useState([])
+
+    useEffect(() => {
+        getCustomers().then(data => {
+            setCustomers(data)
+        })
+    }, [])
+
+
     return (
         <div className="customer-page">
+            <h2>Customer Management</h2>
 
+            <div className="customer-list-container">
+                <h3>Customer List</h3>
+
+                <div className="customer-list">
+                    {customers.map(customer => (
+                        <CustomerCard key={customer.id} customer={customer} setCustomers={setCustomers}/>
+                    ))}
+                </div>
+                
+
+                <div className="add-customer">
+                    add customers
+                </div>
+                
+            </div>
         </div>
     )
 }
