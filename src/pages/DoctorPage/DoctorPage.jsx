5fecdc5216eec0a7911a12180391c444c84be657
Burak Öztaş <143541613+BurakOztas94@users.noreply.github.com>
2024-03-11 20:23:32 +0300

    Update DoctorPage.jsx

--- src/pages/DoctorPage/DoctorPage.jsx
+++ src/pages/DoctorPage/DoctorPage.jsx
@@ -1,11 +1,41 @@
+import { useEffect, useState } from "react"
 import "./DoctorPage.style.css"
+import { getDoctors } from "../../api/DoctorService"
+import DoctorCard from "../../components/Doctor/DoctorCard/DoctorCard"
+import DoctorAdder from "../../components/Doctor/DoctorAdder/DoctorAdder"
+import AvailableDate from "../../components/AvailableDate/AvailableDate"
 
 const DoctorPage = () => {
+    const [doctors, setDoctors] = useState([])
+
+    useEffect(() => {
+        getDoctors().then(data => {
+            setDoctors(data)
+        })
+    }, [])
+
+
     return (
         <div className="doctor-page">
+            <h2>Doctor Management</h2>
 
+            <div className="doctor-list-container">
+                <h3>Doctor List</h3>
+
+                <div className="doctor-list">
+                    {doctors.map(doctor => (
+                        <DoctorCard key={doctor.id} doctor={doctor} setDoctors={setDoctors}/>
+                    ))}
+                </div>
+                
+
+                <DoctorAdder setDoctors={setDoctors}/>
+                
+            </div>
+
+            <AvailableDate />
         </div>
     )
-                
 }
+
 export default DoctorPage
