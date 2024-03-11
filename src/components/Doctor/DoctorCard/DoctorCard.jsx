dd4be3bfe34c131dfa740035afcec2e1d52edfe7
Burak Öztaş <143541613+BurakOztas94@users.noreply.github.com>
2024-03-11 20:23:28 +0300

    Create DoctorCard.jsx

---
+++ src/components/Doctor/DoctorCard/DoctorCard.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react"
+import "./DoctorCard.style.css"
+import { deleteDoctor, updateDoctor } from "../../../api/DoctorService"
+
+const DoctorCard = ({doctor, setDoctors}) => {
+    const [doctorInCard, setDoctorInCard] = useState(doctor)
+
+    useEffect(() => {
+        setDoctorInCard(doctor)
+    }, [doctor])
+
+    const handleUserInput = (event) => {
+        setDoctorInCard({...doctorInCard, [event.target.name]: event.target.value})
+    }
+
+    const handleDoctorUpdate = () => {
+        updateDoctor(doctorInCard).then(data => {
+            setDoctors(prev => [...prev.filter(obj => obj.id !== doctorInCard.id), data])
+        })
+    }
+
+    const handleDoctorDelete = () => {
+        deleteDoctor(doctorInCard.id).then(() => {
+            setDoctors(prev => [...prev.filter(obj => obj.id !== doctorInCard.id)])
+        })
+    }
+
+  return (
+    <div className="doctor-card">
+        <input onChange={handleUserInput} type="text" value={doctorInCard.email} name="email"/>
+        <input onChange={handleUserInput} type="text" value={doctorInCard.name} name="name"/>
+        <input onChange={handleUserInput} type="text" value={doctorInCard.address} name="address"/>
+        <input onChange={handleUserInput} type="text" value={doctorInCard.city} name="city"/>
+        <input onChange={handleUserInput} type="text" value={doctorInCard.phone} name="phone"/>
+
+        <div className="doctor-update-delete">
+            <button onClick={handleDoctorUpdate}>Update</button>
+            <button onClick={handleDoctorDelete}>Delete</button>
+        </div>
+    </div>
+  )
+}
+
+export default DoctorCard
