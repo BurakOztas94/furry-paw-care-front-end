3a174f3e27d695213d919ed7c046d0a61c569f9a
Burak Öztaş <143541613+BurakOztas94@users.noreply.github.com>
2024-03-13 20:04:19 +0300

    Update AppointmentPage.jsx

--- src/pages/AppointmentPage/AppointmentPage.jsx
+++ src/pages/AppointmentPage/AppointmentPage.jsx
@@ -1,11 +1,38 @@
+import { useEffect, useState } from "react"
 import "./AppointmentPage.style.css"
+import { getAppointments } from "../../api/AppointmentService"
+import AppointmentCard from "../../components/Appointment/AppointmentCard/AppointmentCard"
+import AppointmentAdder from "../../components/Appointment/AppointmentAdder/AppointmentAdder"
 
 const AppointmentPage = () => {
-    return (
-        <div className="appoointment-page">
+    const [appointments, setAppointments] = useState([])
 
+    useEffect(() => {
+        getAppointments().then(data => {
+            setAppointments(data)
+        })
+    }, [])
+
+
+    return (
+        <div className="appointment-page">
+            <h2>Appointment Management</h2>
+
+            <div className="appointment-list-container">
+                <h3>Appointment List</h3>
+
+                <div className="appointment-list">
+                    {appointments.map(appointment => (
+                        <AppointmentCard key={appointment.id} appointment={appointment} setAppointments={setAppointments}/>
+                    ))}
+                </div>
+                
+
+                <AppointmentAdder setAppointments={setAppointments}/>
+                
+            </div>
         </div>
     )
-                
 }
+
 export default AppointmentPage
