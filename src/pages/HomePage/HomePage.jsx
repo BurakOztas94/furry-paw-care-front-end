e9b0c4b57f8b940fe38a4d53e8c2adce2332b658
Burak Öztaş <143541613+BurakOztas94@users.noreply.github.com>
2024-03-13 22:47:47 +0300

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,12 @@
-import "./HomePage.style.css"
+import "./HomePage.style.css";
 
 const HomePage = () => {
     return (
         <div className="home-page">
-
+            <h1>Welcome to Furry Paw Vet</h1>
+            <p>This is a sample home page.</p>
         </div>
     )
 }
 
-export default HomePage
+export default HomePage;
