f188b807a8b590bfbcdccd46d726c0ed5dc4a38c
Burak Öztaş <143541613+BurakOztas94@users.noreply.github.com>
2024-03-13 20:33:04 +0300

    Create VaccineService.js

---
+++ src/api/VaccineService.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+export const getVaccines = async () => {
+    const { data } = await axios.get(
+        import.meta.env.VITE_APP_BASE_URL + `vaccines`
+    );
+
+    return data;
+};
+
+export const deleteVaccine = async (id) => {
+    const { data } = await axios.delete(
+        import.meta.env.VITE_APP_BASE_URL + `vaccines/` + id
+    );
+
+    return data;
+};
+
+export const createVaccine = async (vaccine) => {
+    const { data } = await axios.post(
+        import.meta.env.VITE_APP_BASE_URL + `vaccines`,
+        vaccine
+    );
+
+    return data;
+};
+
+export const updateVaccine = async (vaccine) => {
+    const { data } = await axios.put(
+        import.meta.env.VITE_APP_BASE_URL + `vaccines/${vaccine.id}`,
+        vaccine
+    );
+
+    return data;
+};
