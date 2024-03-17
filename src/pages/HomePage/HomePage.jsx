ef8e783faa3aed4dfa41d7c7fa78e43a80b0874c
Burak Öztaş <143541613+BurakOztas94@users.noreply.github.com>
2024-03-17 21:43:21 +0300

    Css added

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,9 +4,14 @@ import furryPawIcon from "../../icons/furry paw care.png";
 const HomePage = () => {
     return (
         <div className="home-page">
+            <h1>Welcome to Furry Paw Vet</h1>           
              <img src={furryPawIcon} alt="Furry Paw Vet Icon" />
-            <h1>Welcome to Furry Paw Vet</h1>
-            <p>This is a sample home page.</p>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt labore itaque explicabo, magni ab iusto, consectetur quo dicta officiis fugit aliquid, odio aperiam doloremque delectus consequuntur at fugiat aut repellat.
+             Laborum consequatur praesentium ipsum mollitia, eveniet facere voluptatibus quia molestiae nihil minus iste magnam distinctio officia in dicta commodi iure necessitatibus ea. Illo, saepe doloribus iusto reiciendis architecto nesciunt voluptate!
+             Facere saepe, dolorem pariatur perspiciatis non voluptatibus atque at, expedita libero reiciendis, ducimus nisi eius! Voluptas nesciunt eligendi nulla. Repudiandae doloremque veniam autem iusto nam ipsa, omnis sequi mollitia aliquid?
+             Fugit impedit qui quidem harum aspernatur perferendis error iure possimus, iusto magni nesciunt, quo, recusandae fuga optio at velit? Numquam facilis fugiat id, iste dicta quis nihil inventore error consectetur.
+             Asperiores qui aspernatur nulla quia iste quisquam esse ipsum unde culpa obcaecati, aut error quidem rerum dicta rem molestiae reprehenderit, excepturi repudiandae. Totam, molestias quibusdam qui velit itaque eligendi fuga.</p>
+            
         </div>
     )
 }
